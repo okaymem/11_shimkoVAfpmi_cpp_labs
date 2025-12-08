b58b3ddf466f5406075e2fe0b25b0623a30757bd
Вова Шимко <vovasimko@MacBook-Air-Vova.local>
2025-12-09 00:06:36 +0300

    some changes

--- semester_1/lab7_class_vector/vector/vector_impl.cpp
+++ semester_1/lab7_class_vector/vector/vector_impl.cpp
@@ -3,10 +3,9 @@
 
     Vector::Vector(): arr_(nullptr), size_(0), capacity_(0){}
 
-    Vector::Vector(int Size):size_(Size), capacity_(Size){
-    arr_ = new int[size_];
-    for(int i = 0; i < size_; ++i){
-        arr_[i] = 0;
+    Vector::Vector(int Size) : size_(std::max(0, Size)), capacity_(size_){
+    if (size_ > 0) {
+        arr_ = new int[size_]();
     }
     }
 
@@ -23,9 +22,12 @@
     }
 
     Vector::Vector(const Vector& other)
-    : size_(other.size_), capacity_(size_), arr_(new int[size_]){
+    : size_(other.size_), capacity_(other.capacity_), arr_(nullptr) {
+    if (capacity_ > 0) {
+        arr_ = new int[capacity_];
         std::copy(other.arr_, other.arr_ + size_, arr_);
     }
+}
     Vector& Vector::operator=(const Vector& other){
         if (this == &other){
             return *this;
@@ -46,13 +48,13 @@
         return arr_[index];
     }
     const int& Vector::At(int index) const{
-        if (index >= size_ ){
+        if (index >= size_ || index < 0){
             throw std::out_of_range("Index out of range!");
         }
         return arr_[index];
     }
     int& Vector::At (int index){
-        if (index >= size_ ){
+        if (index >= size_ || index < 0){
             throw std::out_of_range("Index out of range!");
         }
         return arr_[index];
@@ -66,11 +68,7 @@
     void Vector::PushBack(int value){
         if (size_ == capacity_){
             int newCapacity = capacity_ == 0 ? 1 : capacity_ * 2;
-            int *temp = new int[newCapacity];
-            std::copy(arr_, arr_ + size_, temp);
-            delete[] arr_;
-            arr_ = temp;
-            capacity_ = newCapacity;
+            Reserve(newCapacity);
         } 
         arr_[size_++] = value;
     }
@@ -79,18 +77,20 @@
             size_--;
         }
     }
-
     void Vector::Clear(){
         size_ = 0;
     }
-    void Vector::Reserve(int newCapacity){
-        if (capacity_ < newCapacity){
-            int *temp = new int[newCapacity];
-            std::copy(arr_, arr_ + size_, temp);
-            delete[] arr_;
-            arr_ = temp;
-            capacity_ = newCapacity;
-        }
+    void Vector::Reserve(int newCapacity) {
+    if (newCapacity <= capacity_ || newCapacity < 0) {
+        return;  
+    }
+    int* temp = new int[newCapacity];
+    if (arr_ != nullptr && size_ > 0) {
+        std::copy(arr_, arr_ + size_, temp);
+    }
+    delete[] arr_;  
+    arr_ = temp;
+    capacity_ = newCapacity;
     }
     std::ostream& operator<<(std::ostream& out, const Vector& vec){
         out<<'[';
